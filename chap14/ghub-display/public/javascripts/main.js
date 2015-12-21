b0cdf04d95de82e032ea29ed5a7c09cbf6e97b57
Pascal Bugnion <pascal@bugnion.org>
2015-12-21 16:32:58 +0100

    Chap 14: Restore working JS.

--- chap14/ghub-display/public/javascripts/main.js
+++ chap14/ghub-display/public/javascripts/main.js
@@ -1,10 +1,11 @@
-
-
 (function (requirejs) {
   'use strict';
 
   // -- RequireJS config --
   requirejs.config({
+    // path to the web jars. These definitions allow us 
+    // to use "jquery", rather than "../lib/jquery/jquery",
+    // when defining module dependencies.
     paths: {
       "jquery": "../lib/jquery/jquery",
       "underscore": "../lib/underscorejs/underscore",
@@ -12,10 +13,11 @@
       "nvd3": "../lib/nvd3/nv.d3",
       "bootstrap": "../lib/bootstrap/js/bootstrap"
     },
+
+    shim: {
 // hack to get nvd3 to work with requirejs.
 // see this so question:
-// http://stackoverflow.com/questions/13157704/how-to-integrate-d3-with-require-js#comment32647365_13171592
-    shim: {
+// http://stackoverflow.com/questions/13157704/how-to-integrate-d3-with-require-js#comment32647365_13171592        
       nvd3: {
         deps: ["d3.global"],
         exports: "nv"
@@ -27,13 +29,15 @@
 })(requirejs) ;
 
 // hack to get nvd3 to work with requirejs.
-// see this so question:
+// see this so question on Stack Overflow:
 // http://stackoverflow.com/questions/13157704/how-to-integrate-d3-with-require-js#comment32647365_13171592
 define("d3.global", ["d3"], function(d3global) {
   d3 = d3global;
 });
 
-require(["controller", "responseView"], 
+
+// Initialize the view and controller to bootstrap the application.
+require(["controller", "responseView"],
 function(controller, responseView) {
   controller.initialize();
   responseView.initialize() ;
