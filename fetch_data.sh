a3c28966f4df0a6069f5fc7984ff0ca5e479c738
Pascal Bugnion <pascal@bugnion.org>
2016-01-24 22:25:56 +0000

    Add `fetch_data.sh` bash script to fetch the data.

---
+++ fetch_data.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Fetching data for chapter 6"
+echo
+
+curl --compressed 'data.scala4datascience.com/fec/ohio.csv.gz' | gunzip > chap06/data/ohio.csv
+curl --compressed 'data.scala4datascience.com/fec/us.csv.gz' | gunzip > chap06/data/us.csv
+
+echo
+echo "Fetching data for chapter 10"
+echo
+
+curl --compressed 'data.scala4datascience.com/ling-spam.tar.gz' | tar xzf - -C chap10/data
+
+echo
+echo "Fetching data for chapter 12"
+echo
+
+curl --compressed 'data.scala4datascience.com/ling-spam.tar.gz' | tar xzf - -C chap12/data
